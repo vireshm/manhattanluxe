e178014f7e0e62fd43efaa64b892a285a822b230
vireshm <lanetteam.vireshm@gmail.com>
2017-01-27 10:08:06 +0530

    Changes for Dev

--- html/a.php
+++ html/a.php
@@ -1,2 +1,1 @@
-﻿<?php $str="Hello";  $test="lo";
-echo "tesd"; echo substr_compare($str, $test, -strlen($test), strlen($test)) === 0;  ?>
+﻿<?php $str="Hello";  $test="lo"; echo "tesd"; echo substr_compare($str, $test, -strlen($test), strlen($test)) === 0; echo "Dummy";  ?>
