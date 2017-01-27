3d69c2575fe371a964419c2e241c600525259934
vireshm <lanetteam.vireshm@gmail.com>
2017-01-27 10:19:56 +0530

    Changes for Dev back

--- html/a.php
+++ html/a.php
@@ -1,1 +1,1 @@
-<?php $str="Hello"; echo "tesd"; echo substr_compare($str, $test, -strlen($test), strlen($test)) === 0; echo "Dummy";  ?>
+<?php $str="Hello";  $test="lo"; echo "tesd"; echo substr_compare($str, $test, -strlen($test), strlen($test)) === 0; echo "Dummy";  ?>
